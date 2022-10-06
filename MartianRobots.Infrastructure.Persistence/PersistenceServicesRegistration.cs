1d98bb88b342347f19f4d00d547f287f6b0d1b36
delfinadiaz <delfii.sb@gmail.com>
2022-10-06 13:51:39 -0300

    fixes in set up of heroku connectionstring

--- MartianRobots.Infrastructure.Persistence/PersistenceServicesRegistration.cs
+++ MartianRobots.Infrastructure.Persistence/PersistenceServicesRegistration.cs
@@ -26,13 +26,15 @@ namespace MartianRobots.Infrastructure.Persistence
                 connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
                 var userPassSide = connectionUrl.Split("@")[0];
                 var hostSide = connectionUrl.Split("@")[1];
+                var hostAndPort = hostSide.Split("/")[0];
 
                 var user = userPassSide.Split(":")[0];
                 var password = userPassSide.Split(":")[1];
-                var host = hostSide.Split("/")[0];
+                var host = hostAndPort.Split(":")[0];
+                var port = hostAndPort.Split(":")[1];
                 var database = hostSide.Split("/")[1].Split("?")[0];
 
-                defaultConnectionString = $"Host={host};Database={database};Username={user};Password={password};";
+                defaultConnectionString = $"Host={host};Port={port};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
             }
 
             services.AddDbContext<MartianRobotsDbContext>(options =>
@@ -41,7 +43,7 @@ namespace MartianRobots.Infrastructure.Persistence
 
             var serviceProvider = services.BuildServiceProvider();
             var dbContext = serviceProvider.GetRequiredService<MartianRobotsDbContext>();
-            dbContext.Database.Migrate();
+            //dbContext.Database.Migrate();
 
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddScoped<IMarsRepository, MarsRepository>();
